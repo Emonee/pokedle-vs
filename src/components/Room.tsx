cf7b1d1bb9e297d76d89c94918efa8ff98efbd08
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-01 02:59:58 -0300

    feat: server url

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,3 +1,4 @@
+import { serverUrl } from "../consts/urls";
 import { WebSocketProvider } from "../hooks/useWebSocket";
 import { GameProvider } from "../providers/GameProvider";
 import GamePlays from "./GamePlays";
@@ -12,7 +13,7 @@ export default function Room() {
   const roomName = urlSearchParams.get('room_name')
   const roomId = urlSearchParams.get('room_id')
   return (
-    <WebSocketProvider url={`ws://localhost:5000?user_name=${userName}&room_name=${roomName}&room_id=${roomId}`}>
+    <WebSocketProvider url={`${serverUrl}?user_name=${userName}&room_name=${roomName}&room_id=${roomId}`}>
       <GameProvider>
         <RoomTitle />
         <PlayerList />
---
+++ src/consts/urls.ts
@@ -0,0 +1,1 @@
+export const serverUrl = import.meta.env.PROD ? 'https://python-websocket-hggo.onrender.com/healthz' : 'http://localhost:5000/healthz'
