3cd7f96382fe6d378ed24483e5c7841c302a8d91
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-19 11:25:52 -0300

    change: faster modal

--- client/src/components/Modal.svelte
+++ client/src/components/Modal.svelte
@@ -36,14 +36,14 @@
   /*   Closed state of the dialog   */
   dialog {
     opacity: 0;
-    translate: 0 200px;
+    translate: 0 100px;
     transition:
-      opacity 450ms ease-out,
-      translate 450ms ease-out,
-      overlay 450ms ease-out allow-discrete,
-      display 450ms ease-out allow-discrete;
+      opacity 300ms ease-out,
+      translate 300ms ease-out,
+      overlay 300ms ease-out allow-discrete,
+      display 300ms ease-out allow-discrete;
     /* Equivalent to
-    transition: all 450ms allow-discrete; */
+    transition: all 300ms allow-discrete; */
   }
 
   /*   Before-open state  */
@@ -52,7 +52,7 @@
   @starting-style {
     dialog[open] {
       opacity: 0;
-      translate: 0 200px;
+      translate: 0 100px;
     }
   }
 
@@ -60,11 +60,11 @@
   dialog::backdrop {
     background-color: rgb(0 0 0 / 0%);
     transition:
-      display 450ms allow-discrete,
-      overlay 450ms allow-discrete,
-      background-color 450ms;
+      display 300ms allow-discrete,
+      overlay 300ms allow-discrete,
+      background-color 300ms;
     /* Equivalent to
-    transition: all 450ms allow-discrete; */
+    transition: all 300ms allow-discrete; */
   }
 
   dialog[open]::backdrop {
