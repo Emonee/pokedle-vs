5c628481fa17039bfb379f217a5ab72a759c75d1
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-19 10:11:54 -0300

    feat: basic header

---
+++ client/src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Info from "./Icons/Info.svelte";
+  import InfoModal from "./Modal.svelte";
+</script>
+
+<header class="flex justify-between items-center p-3 shadow relative">
+  <a href={import.meta.env.BASE_URL}>Pokedle VS</a>
+  <InfoModal>
+    {#snippet openDialog(showModal: () => void)}
+      <button onclick={showModal} class="hover:bg-slate-300 rounded-md size-8 text-blue-800"><Info /></button>
+    {/snippet}
+    <p>Pokedle VS  is a replica of <a href="https://pokedle.net/" target="_blank" referrerpolicy="no-referrer">Pokedle</a> but with a virtual opponent.</p>
+    <p>You can play alone or with your friends. Just create a room and let the fun begin.</p>
+    <p>If you want to look at the code, you can find it <a href="https://github.com/emonee/pokedle-vs" target="_blank" referrerpolicy="no-referrer">here</a>.</p>
+    <p>Also, if you want to collaborate, dont doubt to contact me on <a href="https://github.com/emonee" target="_blank" referrerpolicy="no-referrer">Github</a>.</p>
+  </InfoModal>
+</header>
---
+++ client/src/components/Modal.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { HTMLDialogAttributes } from 'svelte/elements';
+  import Close from './Icons/Close.svelte';
+
+  type Props = HTMLDialogAttributes & { openDialog: Snippet<[() => void]> };
+
+  let dialog: HTMLDialogElement;
+  const { children, openDialog }: Props = $props();
+
+  const onclick = (e: MouseEvent) => {
+    const rect = dialog.getBoundingClientRect();
+    const outsideDialog = e.clientX < rect.left || e.clientX > rect.right || e.clientY < rect.top || e.clientY > rect.bottom;
+    if (outsideDialog) dialog.close();
+  }
+  const showModal = () => dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<dialog bind:this={dialog} {onclick} class="p-5 rounded-md relative">
+  <form method="dialog" class="absolute top-2 right-2">
+    <button class="size-7 hover:bg-slate-200 rounded-md"><Close /></button>
+  </form>
+  {@render children?.()}
+</dialog>
+{@render openDialog(showModal)}
+
+<style>
+  /*   Open state of the dialog  */
+  dialog[open] {
+    opacity: 1;
+    translate: 0 0;
+  }
+
+  /*   Closed state of the dialog   */
+  dialog {
+    opacity: 0;
+    translate: 0 200px;
+    transition:
+      opacity 450ms ease-out,
+      translate 450ms ease-out,
+      overlay 450ms ease-out allow-discrete,
+      display 450ms ease-out allow-discrete;
+    /* Equivalent to
+    transition: all 450ms allow-discrete; */
+  }
+
+  /*   Before-open state  */
+  /* Needs to be after the previous dialog[open] rule to take effect,
+      as the specificity is the same */
+  @starting-style {
+    dialog[open] {
+      opacity: 0;
+      translate: 0 200px;
+    }
+  }
+
+  /* Transition the :backdrop when the dialog modal is promoted to the top layer */
+  dialog::backdrop {
+    background-color: rgb(0 0 0 / 0%);
+    transition:
+      display 450ms allow-discrete,
+      overlay 450ms allow-discrete,
+      background-color 450ms;
+    /* Equivalent to
+    transition: all 450ms allow-discrete; */
+  }
+
+  dialog[open]::backdrop {
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+
+  /* This starting-style rule cannot be nested inside the above selector
+  because the nesting selector cannot represent pseudo-elements. */
+
+  @starting-style {
+    dialog[open]::backdrop {
+      background-color: rgb(0 0 0 / 0%);
+    }
+  }
+</style>
