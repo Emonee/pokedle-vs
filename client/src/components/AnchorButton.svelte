baea769ff2c8c2e3b8aa083917349a16a81224ba
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-19 09:19:40 -0300

    feat: Footer

---
+++ client/src/components/AnchorButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { HTMLAnchorAttributes } from 'svelte/elements';
+  const { children, ...attributes }: HTMLAnchorAttributes = $props()
+</script>
+
+<a class="p-2 rounded-md hover:bg-slate-300" { ...attributes }>
+  {@render children?.()}
+</a>
---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import AnchorButton from "./AnchorButton.svelte";
+
+  const root = import.meta.env.BASE_URL;
+  const techs = [
+    { name: 'Vite', url: 'https://vitejs.dev', imgSrc: root + 'img/vite_logo.svg' },
+    { name: 'Svelte', url: 'https://svelte.dev', imgSrc: root + 'img/svelte_logo.png' },
+    { name: 'Tailwind', url: 'https://tailwindcss.com', imgSrc: root + 'img/tailwind_logo.svg' },
+  ]
+</script>
+
+<footer class="flex justify-around flex-wrap gap-x-10 gap-y-2 p-4 shadow-[0px_-1px_6px_0px_#00000017]">
+  <p class="self-center">Made with ❤️ by <a href="https://github.com/emonee" target="_blank" referrerpolicy="no-referrer" class="text-blue-600 hover:text-blue-800">Emone</a></p>
+  <div class="flex gap-2 items-center">
+    <p>Powered with:</p>
+    {#each techs as tech}
+      <AnchorButton href={tech.url} target="_blank" referrerpolicy="no-referrer">
+        <img src={tech.imgSrc} alt={tech.name} class="h-5 w-auto">
+      </AnchorButton>
+    {/each}
+  </div>
+</footer>
